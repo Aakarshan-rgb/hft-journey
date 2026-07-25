c9b1e164df7ac1889c5b5ecc2c326e77f029f66d
Aakarshan <reach4aakarshan@gmail.com>
2026-07-25 16:10:05 +0530

    Update LC_1480_Running_Sum_of_1D_Array.cpp

--- HFT-Journey-v1/HFT-Journey/02_DSA/Arrays/LC_1480_Running_Sum_of_1D_Array.cpp
+++ HFT-Journey-v1/HFT-Journey/02_DSA/Arrays/LC_1480_Running_Sum_of_1D_Array.cpp
@@ -8,3 +8,31 @@ public:
         return nums;
     }
 };
+/*
+==================================================
+Problem: LC 1480  -  Running sum of 1D array
+
+Difficulty: Easy
+
+Topic: Arrays
+
+Pattern: Brute Force
+
+Time Complexity: O(n)
+
+Space Complexity: O(1)
+
+Status: Solved
+
+Revision:
+R1:
+R2:
+R3:
+
+What I Learned:
+- Brute force approach
+
+Date:
+25 July 2026
+==================================================
+*/
